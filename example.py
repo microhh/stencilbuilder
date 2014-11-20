820f4b32327dd8aeff21bc8c3ee22c9d0e32a035
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2014-11-20 19:00:00 +0100

    Fix alignment issue

--- example.py
+++ example.py
@@ -3,11 +3,11 @@
 import numpy as np
 from StencilBuilder import *
 
-u  = Scalar("a")
-v  = Scalar("b")
-w  = Scalar("c")
-ut = Scalar("d")
+u  = Scalar("u")
+v  = Scalar("v")
+w  = Scalar("w")
+ut = Scalar("ut")
 
 print( "duu/dx")
 ut = grad( interp(u) * interp(u) ) + grad( interp(v) * interp(u) ) + grad( interp(w) * interp(u) )
-print("ut[i] = {0};".format(ut.getString(0,9)))
+print("ut[i] = {0};".format(ut.getString(0,8)))
