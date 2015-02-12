75f873871a9f45d1b2bdee2465cc88f907625021
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-02-12 21:48:20 +0100

    More cleanup

--- python/StencilBuilder.py
+++ python/StencilBuilder.py
@@ -37,7 +37,7 @@ class NodeOperator(Node):
       self.loc = right.loc
     elif (type(right) == Scalar):
       self.loc = left.loc
-      
+
     # In case of Vector, only check the k-location
     # CvH types of Vector*Vector will go wrong...
     elif (type(left) == Vector):
@@ -75,8 +75,8 @@ class NodeOperator(Node):
                                         self.right.getString(i, j, k, pad),
                                         ob=ob, os=self.operatorString, cb=cb)
 
-class NodeStencilInterp(Node):
-  def __init__(self, inner, dim):
+class NodeStencilFour(Node):
+  def __init__(self, inner, dim, c0, c1, c2, c3):
     self.inner = inner
     self.depth = inner.depth + 1
     self.pad = 8
@@ -84,6 +84,11 @@ class NodeStencilInterp(Node):
     self.dim = dim
     self.loc = copy.deepcopy(inner.loc)
     self.loc[dim] = not self.loc[dim]
+
+    self.c0 = c0
+    self.c1 = c1
+    self.c2 = c2
+    self.c3 = c3
 
   def getString(self, i, j, k, pad):
     i0 = i1 = i2 = i3 = i
@@ -115,73 +120,21 @@ class NodeStencilInterp(Node):
       lb = ''
       for n in range(2, self.depth):
         lb = lb + '\n'
-      return "{ob}ci0*{0}\n{lb}{ws}+ ci1*{1}\n{lb}{ws}+ ci2*{2}\n{lb}{ws}+ ci3*{3}{cb}".format(
+      return "{ob}{c0}*{0}\n{lb}{ws}+ {c1}*{1}\n{lb}{ws}+ {c2}*{2}\n{lb}{ws}+ {c3}*{3}{cb}".format(
           self.inner.getString(i0, j0, k0, pad),
           self.inner.getString(i1, j1, k1, pad),
           self.inner.getString(i2, j2, k2, pad),
           self.inner.getString(i3, j3, k3, pad),
-          ws=ws, lb=lb, ob=ob, cb=cb)
+          ws=ws, lb=lb, ob=ob, cb=cb,
+          c0=self.c0, c1=self.c1, c2=self.c2, c3=self.c3)
     else:
-      return "{ob}ci0*{0} + ci1*{1} + ci2*{2} + ci3*{3}{cb}".format(
+      return "{ob}{c0}*{0} + {c1}*{1} + {c2}*{2} + {c3}*{3}{cb}".format(
           self.inner.getString(i0, j0, k0, pad),
           self.inner.getString(i1, j1, k1, pad),
           self.inner.getString(i2, j2, k2, pad),
           self.inner.getString(i3, j3, k3, pad),
-          ob=ob, cb=cb)
-
-class NodeStencilGrad(Node):
-  def __init__(self, inner, dim):
-    self.inner = inner
-    self.depth = inner.depth + 1
-    self.pad = 8
-
-    self.dim = dim
-    self.loc = copy.deepcopy(inner.loc)
-    self.loc[dim] = not self.loc[dim]
-
-  def getString(self, i, j, k, pad):
-    i0 = i1 = i2 = i3 = i
-    j0 = j1 = j2 = j3 = j
-    k0 = k1 = k2 = k3 = k
-    if (self.dim == 0):
-      i0 += -1-self.loc[0]
-      i1 +=   -self.loc[0]
-      i2 += +1-self.loc[0]
-      i3 += +2-self.loc[0]
-    elif (self.dim == 1):
-      j0 += -1-self.loc[1]
-      j1 +=   -self.loc[1]
-      j2 += +1-self.loc[1]
-      j3 += +2-self.loc[1]
-    elif (self.dim == 2):
-      k0 += -1-self.loc[2]
-      k1 +=   -self.loc[2]
-      k2 += +1-self.loc[2]
-      k3 += +2-self.loc[2]
-
-    ob = '( '
-    cb = ' )'
-
-    if (self.depth > 1):
-      ws = ''.rjust(pad)
-      pad += self.pad
-
-      lb = ''
-      for n in range(2, self.depth):
-        lb = lb + '\n'
-      return "{ob}cg0*{0}\n{lb}{ws}+ cg1*{1}\n{lb}{ws}+ cg2*{2}\n{lb}{ws}+ cg3*{3}{cb}".format(
-          self.inner.getString(i0, j0, k0, pad),
-          self.inner.getString(i1, j1, k1, pad),
-          self.inner.getString(i2, j2, k2, pad),
-          self.inner.getString(i3, j3, k3, pad),
-          ws=ws, lb=lb, ob=ob, cb=cb)
-    else:
-      return "{ob}cg0*{0} + cg1*{1} + cg2*{2} + cg3*{3}{cb}".format(
-          self.inner.getString(i0, j0, k0, pad),
-          self.inner.getString(i1, j1, k1, pad),
-          self.inner.getString(i2, j2, k2, pad),
-          self.inner.getString(i3, j3, k3, pad),
-          ob=ob, cb=cb)
+          ob=ob, cb=cb,
+          c0=self.c0, c1=self.c1, c2=self.c2, c3=self.c3)
 
 def formatIndex(n, nstr):
   if (n > 0):
@@ -189,7 +142,7 @@ def formatIndex(n, nstr):
   elif (n < 0):
     nn = "-{0}{1}".format(nstr, abs(n))
   else:
-    nn = "    "
+    nn = " " * (len(nstr) + 2)
   return nn
 
 class Field(Node):
@@ -212,7 +165,7 @@ class Vector(Node):
     self.loc   = np.array([0, 0, loc])
 
   def getString(self, i, j, k, pad):
-    kk = formatIndex(k, "kk")
+    kk = formatIndex(k, "")
     return "{0}[k{1}]".format(self.name, kk)
 
 class Scalar(Node):
@@ -225,18 +178,18 @@ class Scalar(Node):
 
 # Define functions.
 def interpx(inner):
-  return NodeStencilInterp(inner, 0)
+  return NodeStencilFour(inner, 0, "ci0", "ci1", "ci2", "ci3")
 def interpy(inner):
-  return NodeStencilInterp(inner, 1)
+  return NodeStencilFour(inner, 1, "ci0", "ci1", "ci2", "ci3")
 def interpz(inner):
-  return NodeStencilInterp(inner, 2)
+  return NodeStencilFour(inner, 2, "ci0", "ci1", "ci2", "ci3")
 
 def gradx(inner):
-  return NodeStencilGrad(inner, 0)
+  return NodeStencilFour(inner, 0, "cg0", "cg1", "cg2", "cg3")
 def grady(inner):
-  return NodeStencilGrad(inner, 1)
+  return NodeStencilFour(inner, 1, "cg0", "cg1", "cg2", "cg3")
 def gradz(inner):
-  return NodeStencilGrad(inner, 2)
+  return NodeStencilFour(inner, 2, "cg0", "cg1", "cg2", "cg3")
 
 def printStencil(lhs, rhs, operator):
   if (not np.array_equal(lhs.loc, rhs.loc)):
