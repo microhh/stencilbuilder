82f44f85d99c25de89160b06814c77e0eccc03c2
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-02-13 16:11:03 +0100

    Increase indentation

--- python/StencilBuilder.py
+++ python/StencilBuilder.py
@@ -13,200 +13,200 @@ zhloc = np.array([None, None, 1])
 
 # Check locations
 def checkLocs(left, right):
-  loc = np.array([ None, None, None ])
-  for i in range(len(loc)):
-    if (left.loc[i] == None and right.loc[i] == None):
-      continue
-    elif (left.loc[i] == None):
-      loc[i] = right.loc[i]
-    elif (right.loc[i] == None):
-      loc[i] = left.loc[i]
-    elif (left.loc[i] != right.loc[i]):
-      raise RuntimeError("Left ({0}) and right ({0}) are not at the same grid location".format(
-        type(left).__name__, type(right).__name__))
-    else:
-      loc[i] = left.loc[i]
+    loc = np.array([ None, None, None ])
+    for i in range(len(loc)):
+        if (left.loc[i] == None and right.loc[i] == None):
+            continue
+        elif (left.loc[i] == None):
+            loc[i] = right.loc[i]
+        elif (right.loc[i] == None):
+            loc[i] = left.loc[i]
+        elif (left.loc[i] != right.loc[i]):
+            raise RuntimeError("Left ({0}) and right ({0}) are not at the same grid location".format(
+                type(left).__name__, type(right).__name__))
+        else:
+            loc[i] = left.loc[i]
 
-  return loc
+    return loc
 
 # Base Node class
 class Node(object):
-  def __add__(self, right):
-    return NodeOperator(self, right, "+")
+    def __add__(self, right):
+        return NodeOperator(self, right, "+")
 
-  def __sub__(self, right):
-    return NodeOperator(self, right, "-")
+    def __sub__(self, right):
+        return NodeOperator(self, right, "-")
 
-  def __mul__(self, right):
-    return NodeOperator(self, right, "*")
+    def __mul__(self, right):
+        return NodeOperator(self, right, "*")
 
 class NodeOperator(Node):
-  def __init__(self, left, right, operatorString):
-    self.left = left
-    self.right = right
-    self.operatorString = operatorString
-    self.depth = max(left.depth, right.depth)
-    if (self.depth > 1):
-      self.pad = 2
-    else:
-      self.pad = 0
+    def __init__(self, left, right, operatorString):
+        self.left = left
+        self.right = right
+        self.operatorString = operatorString
+        self.depth = max(left.depth, right.depth)
+        if (self.depth > 1):
+            self.pad = 2
+        else:
+            self.pad = 0
 
-    self.loc = checkLocs(left, right)
+        self.loc = checkLocs(left, right)
 
-  def getString(self, i, j, k, pad, plane):
-    ob = '( '
-    cb = ' )'
+    def getString(self, i, j, k, pad, plane):
+        ob = '( '
+        cb = ' )'
 
-    if (self.depth > 1):
-      ws = ''.rjust(pad)
-      pad += self.pad
+        if (self.depth > 1):
+            ws = ''.rjust(pad)
+            pad += self.pad
 
-      lb = ''
-      for n in range(1, self.depth):
-        lb = lb + '\n'
+            lb = ''
+            for n in range(1, self.depth):
+                lb = lb + '\n'
 
-      return "{ob}{0}\n{lb}{ws}{os} {1}{cb}".format(self.left.getString(i, j, k, pad, plane),
-                                                    self.right.getString(i, j, k, pad, plane),
-                                                    ws=ws, lb=lb, ob=ob,
-                                                    os=self.operatorString, cb=cb)
-    else:
-      return "{ob}{0} {os} {1}{cb}".format(self.left.getString(i, j, k, pad, plane),
-                                           self.right.getString(i, j, k, pad, plane),
-                                           ob=ob, os=self.operatorString, cb=cb)
+            return "{ob}{0}\n{lb}{ws}{os} {1}{cb}".format(self.left.getString(i, j, k, pad, plane),
+                                                          self.right.getString(i, j, k, pad, plane),
+                                                          ws=ws, lb=lb, ob=ob,
+                                                          os=self.operatorString, cb=cb)
+        else:
+            return "{ob}{0} {os} {1}{cb}".format(self.left.getString(i, j, k, pad, plane),
+                                                 self.right.getString(i, j, k, pad, plane),
+                                                 ob=ob, os=self.operatorString, cb=cb)
 
 class NodeStencilFour(Node):
-  def __init__(self, inner, dim, c0, c1, c2, c3):
-    self.inner = inner
-    self.depth = inner.depth + 1
-    self.pad = 6
+    def __init__(self, inner, dim, c0, c1, c2, c3):
+        self.inner = inner
+        self.depth = inner.depth + 1
+        self.pad = 6
 
-    self.dim = dim
-    self.loc = np.copy(inner.loc)
-    self.loc[dim] = not self.loc[dim]
+        self.dim = dim
+        self.loc = np.copy(inner.loc)
+        self.loc[dim] = not self.loc[dim]
 
-    self.c0 = c0
-    self.c1 = c1
-    self.c2 = c2
-    self.c3 = c3
+        self.c0 = c0
+        self.c1 = c1
+        self.c2 = c2
+        self.c3 = c3
 
-  def getString(self, i, j, k, pad, plane):
-    i0 = i1 = i2 = i3 = i
-    j0 = j1 = j2 = j3 = j
-    k0 = k1 = k2 = k3 = k
-    if (self.dim == 0):
-      i0 += -1-self.loc[0]
-      i1 +=   -self.loc[0]
-      i2 += +1-self.loc[0]
-      i3 += +2-self.loc[0]
-    elif (self.dim == 1):
-      j0 += -1-self.loc[1]
-      j1 +=   -self.loc[1]
-      j2 += +1-self.loc[1]
-      j3 += +2-self.loc[1]
-    elif (self.dim == 2):
-      k0 += -1-self.loc[2]
-      k1 +=   -self.loc[2]
-      k2 += +1-self.loc[2]
-      k3 += +2-self.loc[2]
+    def getString(self, i, j, k, pad, plane):
+        i0 = i1 = i2 = i3 = i
+        j0 = j1 = j2 = j3 = j
+        k0 = k1 = k2 = k3 = k
+        if (self.dim == 0):
+            i0 += -1-self.loc[0]
+            i1 +=   -self.loc[0]
+            i2 += +1-self.loc[0]
+            i3 += +2-self.loc[0]
+        elif (self.dim == 1):
+            j0 += -1-self.loc[1]
+            j1 +=   -self.loc[1]
+            j2 += +1-self.loc[1]
+            j3 += +2-self.loc[1]
+        elif (self.dim == 2):
+            k0 += -1-self.loc[2]
+            k1 +=   -self.loc[2]
+            k2 += +1-self.loc[2]
+            k3 += +2-self.loc[2]
 
-    ob = '( '
-    cb = ' )'
+        ob = '( '
+        cb = ' )'
 
-    newplane = np.copy(plane)
-    if (self.depth-1 < 2):
-      newplane[self.dim] = 1
+        newplane = np.copy(plane)
+        if (self.depth-1 < 2):
+            newplane[self.dim] = 1
 
-    if (self.depth > 1):
+        if (self.depth > 1):
 
-      ws = ''.rjust(pad)
-      pad += self.pad
+            ws = ''.rjust(pad)
+            pad += self.pad
 
-      lb = ''
-      for n in range(2, self.depth):
-        lb = lb + '\n'
-      return "{ob}{c0}*{0}\n{lb}{ws}+ {c1}*{1}\n{lb}{ws}+ {c2}*{2}\n{lb}{ws}+ {c3}*{3}{cb}".format(
-          self.inner.getString(i0, j0, k0, pad, newplane),
-          self.inner.getString(i1, j1, k1, pad, newplane),
-          self.inner.getString(i2, j2, k2, pad, newplane),
-          self.inner.getString(i3, j3, k3, pad, newplane),
-          ws=ws, lb=lb, ob=ob, cb=cb,
-          c0=self.c0, c1=self.c1, c2=self.c2, c3=self.c3)
-    else:
-      return "{ob}{c0}*{0} + {c1}*{1} + {c2}*{2} + {c3}*{3}{cb}".format(
-          self.inner.getString(i0, j0, k0, pad, newplane),
-          self.inner.getString(i1, j1, k1, pad, newplane),
-          self.inner.getString(i2, j2, k2, pad, newplane),
-          self.inner.getString(i3, j3, k3, pad, newplane),
-          ob=ob, cb=cb,
-          c0=self.c0, c1=self.c1, c2=self.c2, c3=self.c3)
+            lb = ''
+            for n in range(2, self.depth):
+                lb = lb + '\n'
+            return "{ob}{c0}*{0}\n{lb}{ws}+ {c1}*{1}\n{lb}{ws}+ {c2}*{2}\n{lb}{ws}+ {c3}*{3}{cb}".format(
+                self.inner.getString(i0, j0, k0, pad, newplane),
+                self.inner.getString(i1, j1, k1, pad, newplane),
+                self.inner.getString(i2, j2, k2, pad, newplane),
+                self.inner.getString(i3, j3, k3, pad, newplane),
+                ws=ws, lb=lb, ob=ob, cb=cb,
+                c0=self.c0, c1=self.c1, c2=self.c2, c3=self.c3)
+        else:
+            return "{ob}{c0}*{0} + {c1}*{1} + {c2}*{2} + {c3}*{3}{cb}".format(
+                self.inner.getString(i0, j0, k0, pad, newplane),
+                self.inner.getString(i1, j1, k1, pad, newplane),
+                self.inner.getString(i2, j2, k2, pad, newplane),
+                self.inner.getString(i3, j3, k3, pad, newplane),
+                ob=ob, cb=cb,
+                c0=self.c0, c1=self.c1, c2=self.c2, c3=self.c3)
 
 def formatIndex(n, nstr):
-  if (n > 0):
-    nn = "+{0}{1}".format(nstr, abs(n))
-  elif (n < 0):
-    nn = "-{0}{1}".format(nstr, abs(n))
-  else:
-    nn = " " * (len(nstr) + 2)
-  return nn
+    if (n > 0):
+        nn = "+{0}{1}".format(nstr, abs(n))
+    elif (n < 0):
+        nn = "-{0}{1}".format(nstr, abs(n))
+    else:
+        nn = " " * (len(nstr) + 2)
+    return nn
 
 class Field(Node):
-  def __init__(self, name, loc):
-    self.name = name
-    self.depth = 0
-    self.loc = np.copy(loc)
+    def __init__(self, name, loc):
+        self.name = name
+        self.depth = 0
+        self.loc = np.copy(loc)
 
-  def getString(self, i, j, k, pad, plane):
-    compact = True
-    ii = jj = kk = ""
-    if (plane[0] or not compact):
-      ii = formatIndex(i, "ii")
-    if (plane[1] or not compact):
-      jj = formatIndex(j, "jj")
-    if (plane[2] or not compact):
-      kk = formatIndex(k, "kk")
-    return "{0}[ijk{1}{2}{3}]".format(self.name, ii, jj, kk)
+    def getString(self, i, j, k, pad, plane):
+        compact = True
+        ii = jj = kk = ""
+        if (plane[0] or not compact):
+            ii = formatIndex(i, "ii")
+        if (plane[1] or not compact):
+            jj = formatIndex(j, "jj")
+        if (plane[2] or not compact):
+            kk = formatIndex(k, "kk")
+        return "{0}[ijk{1}{2}{3}]".format(self.name, ii, jj, kk)
 
 # Vector class representing a profile
 class Vector(Node):
-  def __init__(self, name, loc):
-    self.name = name
-    self.depth = 0
-    self.loc = np.copy(loc)
+    def __init__(self, name, loc):
+        self.name = name
+        self.depth = 0
+        self.loc = np.copy(loc)
 
-  def getString(self, i, j, k, pad, plane):
-    kk = formatIndex(k, "")
-    return "{0}[k{1}]".format(self.name, kk)
+    def getString(self, i, j, k, pad, plane):
+        kk = formatIndex(k, "")
+        return "{0}[k{1}]".format(self.name, kk)
 
 class Scalar(Node):
-  def __init__(self, name):
-    self.name = name
-    self.depth = 0
-    self.loc = np.array([ None, None, None ])
+    def __init__(self, name):
+        self.name = name
+        self.depth = 0
+        self.loc = np.array([ None, None, None ])
 
-  def getString(self, i, j, k, pad, plane):
-    return "{0}".format(self.name)
+    def getString(self, i, j, k, pad, plane):
+        return "{0}".format(self.name)
 
 # Define functions.
 def interpx(inner):
-  return NodeStencilFour(inner, 0, "ci0", "ci1", "ci2", "ci3")
+    return NodeStencilFour(inner, 0, "ci0", "ci1", "ci2", "ci3")
 def interpy(inner):
-  return NodeStencilFour(inner, 1, "ci0", "ci1", "ci2", "ci3")
+    return NodeStencilFour(inner, 1, "ci0", "ci1", "ci2", "ci3")
 def interpz(inner):
-  return NodeStencilFour(inner, 2, "ci0", "ci1", "ci2", "ci3")
+    return NodeStencilFour(inner, 2, "ci0", "ci1", "ci2", "ci3")
 
 def gradx(inner):
-  return NodeStencilFour(inner, 0, "cg0", "cg1", "cg2", "cg3")
+    return NodeStencilFour(inner, 0, "cg0", "cg1", "cg2", "cg3")
 def grady(inner):
-  return NodeStencilFour(inner, 1, "cg0", "cg1", "cg2", "cg3")
+    return NodeStencilFour(inner, 1, "cg0", "cg1", "cg2", "cg3")
 def gradz(inner):
-  return NodeStencilFour(inner, 2, "cg0", "cg1", "cg2", "cg3")
+    return NodeStencilFour(inner, 2, "cg0", "cg1", "cg2", "cg3")
 
 def printStencil(lhs, rhs, operator):
-  checkLocs(lhs, rhs)
+    checkLocs(lhs, rhs)
 
-  index = "[ijk]"
-  indent = len(lhs.name) + len(index) + len(operator) + 2
+    index = "[ijk]"
+    indent = len(lhs.name) + len(index) + len(operator) + 2
 
-  plane = np.array([0,0,0])
-  print("{0}{1} {2} {3};".format(lhs.name, index, operator, rhs.getString(0, 0, 0, indent, plane)))
+    plane = np.array([0,0,0])
+    print("{0}{1} {2} {3};".format(lhs.name, index, operator, rhs.getString(0, 0, 0, indent, plane)))
 
--- python/parser.py
+++ python/parser.py
@@ -8,11 +8,11 @@ from StencilBuilder import *
 # Retrieve the file from the command line arguments.
 argc = len(sys.argv)
 if (not 2 <= argc < 4):
-  raise RuntimeError("Illegal number of arguments")
+    raise RuntimeError("Illegal number of arguments")
 else:
-  inputfile = str(sys.argv[1])
-  if (argc == 3):
-    outputfile = str(sys.argv[2])
+    inputfile = str(sys.argv[1])
+    if (argc == 3):
+        outputfile = str(sys.argv[2])
 
 # Save a backup of the original file.
 shutil.copyfile(inputfile, "{0}{1}".format(inputfile, ".orig"))
@@ -24,7 +24,7 @@ f.close()
 
 # Remove the line breaks.
 for n in range(len(lines)):
-  lines[n] = lines[n].rstrip()
+    lines[n] = lines[n].rstrip()
 
 record = False
 blocks = []
@@ -35,64 +35,64 @@ names = []
 
 # Loop over lines and store blocks of StencilBuilder code.
 for n in lines:
-  lineindex += 1
-  line = n.find("//$")
+    lineindex += 1
+    line = n.find("//$")
 
-  # Process the keyword
-  if(line != -1):
-    indent = line
-    kw = n[line + 3::].strip().split(' ')
-    if(kw[0] == "SBStart"):
-      startline = lineindex
-      record = True
-      continue
+    # Process the keyword
+    if(line != -1):
+        indent = line
+        kw = n[line + 3::].strip().split(' ')
+        if(kw[0] == "SBStart"):
+            startline = lineindex
+            record = True
+            continue
 
-    elif(kw[0] == "SBEnd"):
-      endline = lineindex
-      record = False
-      blocks.append((block, startline, endline, indent))
-      block = []
-      names = []
-      continue
+        elif(kw[0] == "SBEnd"):
+            endline = lineindex
+            record = False
+            blocks.append((block, startline, endline, indent))
+            block = []
+            names = []
+            continue
 
-    else:
-      raise RuntimeError("Syntax error in StencilBuilder tags")
+        else:
+            raise RuntimeError("Syntax error in StencilBuilder tags")
 
-  if(record):
-    block.append(n.strip()+'\n')
+    if(record):
+        block.append(n.strip()+'\n')
 
 # Execute the blocks in reverse order
 blocks.reverse()
 
 for n in blocks:
-  # Get the code block.
-  code = ''.join(n[0])
+    # Get the code block.
+    code = ''.join(n[0])
 
-  # Execute the code block and capture the output by temporarily
-  # redirecting the stdout.
-  buffer = StringIO.StringIO()
-  sys.stdout = buffer
-  exec(code)
-  sys.stdout = sys.__stdout__
-  output = buffer.getvalue().splitlines()
-  del(code)
-  del(buffer)
+    # Execute the code block and capture the output by temporarily
+    # redirecting the stdout.
+    buffer = StringIO.StringIO()
+    sys.stdout = buffer
+    exec(code)
+    sys.stdout = sys.__stdout__
+    output = buffer.getvalue().splitlines()
+    del(code)
+    del(buffer)
 
-  # Add the indentation.
-  for l in range(len(output)):
-    output[l] = '{0}{1}'.format(' ' * n[3], output[l])
+    # Add the indentation.
+    for l in range(len(output)):
+        output[l] = '{0}{1}'.format(' ' * n[3], output[l])
 
-  # Delete the StencilBuilder lines.
-  del(lines[n[1]:n[2]+1])
+    # Delete the StencilBuilder lines.
+    del(lines[n[1]:n[2]+1])
 
-  # Replace it with the new code.
-  lines[n[1]:n[1]] = output
+    # Replace it with the new code.
+    lines[n[1]:n[1]] = output
 
 if (argc == 2):
-  for n in lines:
-    print("{0}".format(n))
+    for n in lines:
+        print("{0}".format(n))
 elif (argc == 3):
-  f = file("{0}".format(outputfile), "w")
-  for n in lines:
-    f.write("{0}\n".format(n))
-  f.close()
+    f = file("{0}".format(outputfile), "w")
+    for n in lines:
+        f.write("{0}\n".format(n))
+    f.close()
