da8464e3f78e280d4fc089efbb15e272bae167e9
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-08-03 15:26:05 +0200

    Added Reynold stresses to the stencil builder

---
+++ python/reynolds_stress.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+from StencilBuilder import *
+
+u  = Field("u" , uloc )
+v  = Field("v" , vloc )
+w  = Field("w" , wloc )
+wx = Field("wx", uwloc)
+
+umean = Vector("umean", zloc)
+vmean = Vector("vmean", zloc)
+
+uw_tend = Field("uw_tend", uwloc)
+
+dxi = Scalar("dxi")
+dyi = Scalar("dyi")
+
+visc = Scalar("visc")
+
+dzi4  = Vector("dzi4" , zloc )
+dzhi4 = Vector("dzhi4", zhloc)
+
+rhs_turb  = gradz( interpz(wx) * (u-umean) ) * dzhi4
+rhs_shear = wx * gradz(umean) * dzhi4
+rhs_diss  = visc * ( gradx(interpz(interpx(u-umean))) * dxi   * gradx(interpz(interpy(v-vmean))) * dxi \
+                   + grady(interpz(interpy(u-umean))) * dyi   * grady(interpz(interpx(v-vmean))) * dyi \
+                   + gradz(               (u-umean) ) * dzhi4 * gradz(interpx(interpy(v-vmean))) * dzhi4 )
+
+uw_rhs = rhs_diss
+
+#printStencil(uw_tend, uw_rhs, "-=", "bot+1")
+#printEmptyLine(3)
+printStencil(uw_tend, uw_rhs, "-=", "int")
+#printEmptyLine(3)
+#printStencil(uw_tend, uw_rhs, "-=", "top-1")
