39e39d416ab2b05af39de780c26e63e915b0769a
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-08-04 23:26:44 +0200

    Added additional budget terms

--- python/reynolds_stress.py
+++ python/reynolds_stress.py
@@ -2,6 +2,7 @@
 
 from StencilBuilder import *
 
+p  = Field("p" , sloc )
 u  = Field("u" , uloc )
 v  = Field("v" , vloc )
 w  = Field("w" , wloc )
@@ -10,7 +11,7 @@ wx = Field("wx", uwloc)
 umean = Vector("umean", zloc)
 vmean = Vector("vmean", zloc)
 
-uw_diss = Field("uw_diss", uwloc)
+uw_pres = Field("uw_pres", uwloc)
 
 dxi = Scalar("cgi*dxi")
 dyi = Scalar("cgi*dyi")
@@ -24,32 +25,26 @@ dzhi4 = Vector("dzhi4", zhloc)
 rhs_turb  = gradz( interpz(wx) * (u-umean) ) * dzhi4
 rhs_shear = wx * gradz(umean) * dzhi4
 
-rhs_diss_pseudo_x = two * visc * ( gradx(interpxz(u-umean)) * dxi   * gradx(interpyz(v-vmean)) * dxi   )
-rhs_diss_pseudo_y = two * visc * ( grady(interpyz(u-umean)) * dyi   * grady(interpxz(v-vmean)) * dyi   )
-rhs_diss_pseudo_z = two * visc * ( gradz(        (u-umean)) * dzhi4 * gradz(interpxy(v-vmean)) * dzhi4 )
+rhs_diss_pseudo_x = ( gradx(interpxz(u-umean)) * dxi   * gradx(w) * dxi   )
+rhs_diss_pseudo_y = ( grady(interpyz(u-umean)) * dyi   * grady(interpxy(w)) * dyi   )
+rhs_diss_pseudo_z = ( gradz(        (u-umean)) * dzhi4 * gradz(interpxz(w)) * dzhi4 )
 
-rhs_diss_x1 = gradx( interpxz( u-umean ) ) * dxi \
-            * ( gradx( interpyz( v-vmean ) ) * dxi + grady( interpyz( u-umean ) ) * dyi )
-rhs_diss_y1 = grady( interpyz( u-umean ) ) * dyi \
-            * two * grady( interpxz( v-vmean ) ) * dyi
-rhs_diss_z1 = gradz( u-umean ) * dzhi4 \
-            * ( gradz( interpxy( v-vmean ) ) * dzhi4 + grady( interpxy( w ) ) * dyi )
+rhs_rdstr = interpxz(p) * ( gradz(u-umean) * dzhi4 + gradx(w) * dxi )
 
-rhs_diss_x2 = gradx( interpyz( v-vmean ) ) * dxi \
-            * two * gradx( interpxz( u-umean ) ) * dxi
-rhs_diss_y2 = grady( interpxz( v-vmean ) ) * dyi \
-            * ( grady( interpyz( u-umean ) ) * dyi + gradx( interpyz( v-vmean ) ) * dxi )
-rhs_diss_z2 = gradz( interpxy( v-vmean ) ) * dzhi4 \
-            * ( gradz( u-umean ) * dzhi4 + gradx( w ) * dxi )
+rhs_pres = gradz( (u-umean) * interpx(p) ) * dzhi4 + gradx( w * interpz(p) ) * dxi
 
-printStencil(uw_diss, rhs_diss_x1, "-=", "int", "[k]")
-printEmptyLine(2)
-printStencil(uw_diss, rhs_diss_y1, "-=", "int", "[k]")
-printEmptyLine(2)
-printStencil(uw_diss, rhs_diss_z1, "-=", "int", "[k]")
-printEmptyLine(2)
-printStencil(uw_diss, rhs_diss_x2, "-=", "int", "[k]")
-printEmptyLine(2)
-printStencil(uw_diss, rhs_diss_y2, "-=", "int", "[k]")
-printEmptyLine(2)
-printStencil(uw_diss, rhs_diss_z2, "-=", "int", "[k]")
+#rhs_diss_x1 = gradx( interpxz( u-umean ) ) * dxi \
+#            * ( gradx( interpyz( v-vmean ) ) * dxi + grady( interpyz( u-umean ) ) * dyi )
+#rhs_diss_y1 = grady( interpyz( u-umean ) ) * dyi \
+#            * two * grady( interpxz( v-vmean ) ) * dyi
+#rhs_diss_z1 = gradz( u-umean ) * dzhi4 \
+#            * ( gradz( interpxy( v-vmean ) ) * dzhi4 + grady( interpxy( w ) ) * dyi )
+#
+#rhs_diss_x2 = gradx( interpyz( v-vmean ) ) * dxi \
+#            * two * gradx( interpxz( u-umean ) ) * dxi
+#rhs_diss_y2 = grady( interpxz( v-vmean ) ) * dyi \
+#            * ( grady( interpyz( u-umean ) ) * dyi + gradx( interpyz( v-vmean ) ) * dxi )
+#rhs_diss_z2 = gradz( interpxy( v-vmean ) ) * dzhi4 \
+#            * ( gradz( u-umean ) * dzhi4 + gradx( w ) * dxi )
+
+printStencil(uw_pres, rhs_pres, "-=", "int", "[k]")
