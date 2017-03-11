aa521ca8ed220dc5711b9b515481e014b1c0d89c
Jeremy Gibbs <gibbz@ou.edu>
2017-03-10 23:40:06 -0600

    Fixed to account for averaging over horizontal planes. Terms split to ensure consistency with non-slope counterparts.

--- python/ubflux_slope.py
+++ python/ubflux_slope.py
@@ -17,60 +17,34 @@ pmean    = Vector("pmean", zloc)
 visc     = Scalar("visc")
 sinalpha = Scalar("sin(alpha)")
 cosalpha = Scalar("cos(alpha)")
-n2       = Scalar("n2")
+N2       = Scalar("N2")
 dxi      = Scalar("cgi*dxi")
 dyi      = Scalar("cgi*dyi")
 
 dzi4     = Vector("dzi4" , zloc )
 dzhi4    = Vector("dzhi4", zhloc)
 
-bu_advec = Field("bu_advec", uloc)
 bu_shear = Field("bu_shear", uloc)
 bu_turb  = Field("bu_turb" , uloc)
 bu_buoy  = Field("bu_buoy" , uloc)
+bu_strat = Field("bu_strat", uloc)
 bu_visc  = Field("bu_visc" , uloc)
-bu_pres  = Field("bu_pres" , uloc)
+bu_diss  = Field("bu_diss" , uloc)
 
-rhs_advec = umean * gradx( interpx(u-umean) * (b-bmean)) * dxi \
-          + vmean * grady( interpy(u-umean) * interpxy(b-bmean)) * dyi# \
-          #+ w * gradz( (u-umean)*(b-bmean)) * dzih4
-
-rhs_shear = (u-umean)*interpx(b-bmean)*gradx(umean)*dxi \
-          + interpxy(v-vmean)*interpx(b-bmean)*grady(interpy(umean))*dyi \
-          + interpxz(w)*interpx(b-bmean)*gradz(interpz(umean))*dzi4 \
-          + (u-umean)**2 * gradx(bmean)*dxi \
-          + (u-umean)*interpxy(v-vmean) * grady(interpxy(bmean))*dyi \
+rhs_shear = interpxz(w)*interpx(b-bmean)*gradz(interpz(umean))*dzi4 \
           + (u-umean)*interpxz(w) * gradz(interpxz(bmean))*dzi4
 
-rhs_turb = gradx( interpx(u-umean)**2 * (b-bmean) ) * dxi \
-         + grady( interpx(v-vmean) * interpy(u-umean) * interpxy(b-bmean) ) * dyi \
-         + gradz( interpz(u-umean) * interpx(w) * interpxz(b-bmean) ) * dzi4
+rhs_turb = gradz( interpz(u-umean) * interpx(w) * interpxz(b-bmean) ) * dzi4
 
-rhs_buoy = interpx( b-bmean )**2 * sinalpha \
-         - n2*((u-umean)**2 * sinalpha \
-             + (u-umean)*interpxy(v-vmean) \
-             + (u-umean)*interpxz(w)*cosalpha) 
- 
-rhs_visc = visc * ( interpx(b-bmean)*gradx(gradx((u-umean)) * dxi) * dxi \
-                  + interpx(b-bmean)*grady(grady((u-umean)) * dyi) * dyi \
-                  + interpx(b-bmean)*gradz(gradz((u-umean)) * dzhi4) * dzi4 \
-                  + (u-umean)*gradx(gradx(interpx(b-bmean))*dxi)*dxi \
-                  + (u-umean)*grady(grady(interpx(b-bmean))*dyi)*dyi \
-                  + (u-umean)*gradz(gradz(interpx(b-bmean))*dzhi4)*dzi4)
- 
-rhs_pres = interpx(b-bmean) * gradx( (p-pmean) ) * dxi
+rhs_buoy = interpx( b-bmean )**2 * sinalpha 
 
-printStencil(bu_advec, rhs_advec, "-=", "bot", "[k]")
-printEmptyLine(3)
-printStencil(bu_advec, rhs_advec, "-=", "bot+1", "[k]")
-printEmptyLine(3)
-printStencil(bu_advec, rhs_advec, "-=", "int", "[k]")
-printEmptyLine(3)
-printStencil(bu_advec, rhs_advec, "-=", "top-1", "[k]")
-printEmptyLine(3)
-printStencil(bu_advec, rhs_advec, "-=", "top", "[k]")
+rhs_strat = N2*((u-umean)**2 * sinalpha + (u-umean)*interpxz(w)*cosalpha)
 
-printEmptyLine(6)
+rhs_visc = visc * gradz(gradz((u-umean) * interpx(b-bmean) ) * dzhi4) * dzi4
+
+rhs_diss = 2.*visc * ( gradx( interpx(u-umean) ) * dxi  * gradx( (b-bmean) )*dxi \
+                     + grady( interpy(u-umean) ) * dyi  * grady( interpxy(b-bmean) )*dyi \
+                     + gradz( interpz(u-umean) ) * dzi4 * gradz( interpxz(b-bmean) )*dzi4)
 
 printStencil(bu_shear, rhs_shear, "-=", "int", "[k]")
 
@@ -92,6 +66,10 @@ printStencil(bu_buoy, rhs_buoy, "+=", "int", "[k]")
 
 printEmptyLine(6)
 
+printStencil(bu_strat, rhs_strat, "-=", "int", "[k]")
+
+printEmptyLine(6)
+
 printStencil(bu_visc, rhs_visc, "+=", "bot", "[k]")
 printEmptyLine(3)
 printStencil(bu_visc, rhs_visc, "+=", "bot+1", "[k]")
@@ -108,4 +86,12 @@ printStencil(bu_visc, rhs_visc, "+=", "top", "[k]")
 
 printEmptyLine(6)
 
-printStencil(bu_pres, rhs_pres, "-=", "int", "[k]")
+printStencil(bu_diss, rhs_diss, "-=", "bot", "[k]")
+printEmptyLine(3)
+printStencil(bu_diss, rhs_diss, "-=", "bot+1", "[k]")
+printEmptyLine(3)
+printStencil(bu_diss, rhs_diss, "-=", "int", "[k]")
+printEmptyLine(3)
+printStencil(bu_diss, rhs_diss, "-=", "top-1", "[k]")
+printEmptyLine(3)
+printStencil(bu_diss, rhs_diss, "-=", "top", "[k]")
--- python/vbflux_slope.py
+++ python/vbflux_slope.py
@@ -17,59 +17,31 @@ pmean    = Vector("pmean", zloc)
 visc     = Scalar("visc")
 sinalpha = Scalar("sin(alpha)")
 cosalpha = Scalar("cos(alpha)")
-n2       = Scalar("n2")
+N2       = Scalar("N2")
 dxi      = Scalar("cgi*dxi")
 dyi      = Scalar("cgi*dyi")
 
 dzi4     = Vector("dzi4" , zloc )
 dzhi4    = Vector("dzhi4", zhloc)
 
-bv_advec = Field("bv_advec", vloc)
 bv_shear = Field("bv_shear", vloc)
 bv_turb  = Field("bv_turb" , vloc)
-bv_buoy  = Field("bv_buoy" , vloc)
+bv_strat = Field("bv_strat", vloc)
 bv_visc  = Field("bv_visc" , vloc)
-bv_pres  = Field("bv_pres" , vloc)
+bv_diss  = Field("bv_diss" , vloc)
 
-rhs_advec = umean * gradx( interpx(v-vmean) * interpxy(b-bmean)) * dxi \
-          + vmean * grady( interpy(v-vmean) * (b-bmean)) * dyi# \
-          #+ w * gradz( (u-umean)*(b-bmean)) * dzih4
+rhs_shear = interpyz(w)*interpy(b-bmean)*gradz(interpz(vmean))*dzi4 \
+          + (v-umean)*interpyz(w) * gradz(interpyz(bmean))*dzi4
 
-rhs_shear = interpxy(u-umean)*interpy(b-bmean)*gradx(interpx(vmean))*dxi \
-          + (v-vmean)*interpy(b-bmean)*grady(vmean)*dyi \
-          + interpyz(w)*interpy(b-bmean)*gradz(interpz(vmean))*dzi4 \
-          + (v-vmean)*interpxy(u-umean) * gradx(interpxy(bmean))*dxi \
-           + (v-umean)**2 * grady(bmean)*dyi \
-           + (v-umean)*interpyz(w) * gradz(interpyz(bmean))*dzi4
+rhs_turb = gradz( interpz(v-vmean) * interpy(w) * interpyz(b-bmean) ) * dzi4
 
-rhs_turb = gradx( interpx(v-vmean)*interpy(u-umean)*interpxy(b-bmean) ) * dxi \
-         + grady( interpy(v-vmean)**2  * (b-bmean) ) * dyi \
-         + gradz( interpz(v-vmean) * interpy(w) * interpyz(b-bmean) ) * dzi4
- 
-rhs_buoy = n2*( (v-vmean)*interpxy(u-umean) * sinalpha \
-              + (v-vmean)**2 \
-              + (v-vmean)*interpyz(w)*cosalpha ) 
- 
-rhs_visc = visc * ( interpy(b-bmean)*gradx(gradx((v-vmean)) * dxi) * dxi \
-                  + interpy(b-bmean)*grady(grady((v-vmean)) * dyi) * dyi \
-                  + interpy(b-bmean)*gradz(gradz((v-vmean)) * dzhi4) * dzi4 \
-                  + (v-vmean)*gradx(gradx(interpy(b-bmean))*dxi)*dxi \
-                  + (v-vmean)*grady(grady(interpy(b-bmean))*dyi)*dyi \
-                  + (v-vmean)*gradz(gradz(interpy(b-bmean))*dzhi4)*dzi4)
-  
-rhs_pres = interpy(b-bmean) * grady( (p-pmean) ) * dyi
+rhs_strat = N2*( (v-vmean)*interpxy(u-umean)*sinalpha + (v-vmean)*interpyz(w)*cosalpha ) 
 
-printStencil(bv_advec, rhs_advec, "-=", "bot", "[k]")
-printEmptyLine(3)
-printStencil(bv_advec, rhs_advec, "-=", "bot+1", "[k]")
-printEmptyLine(3)
-printStencil(bv_advec, rhs_advec, "-=", "int", "[k]")
-printEmptyLine(3)
-printStencil(bv_advec, rhs_advec, "-=", "top-1", "[k]")
-printEmptyLine(3)
-printStencil(bv_advec, rhs_advec, "-=", "top", "[k]")
+rhs_visc = visc * gradz(gradz((v-umean) * interpy(b-bmean) ) * dzhi4) * dzi4
 
-printEmptyLine(6)
+rhs_diss = 2.*visc * ( gradx( interpx(v-vmean) ) * dxi  * gradx( interpxy(b-bmean) )*dxi \
+                     + grady( interpy(v-vmean) ) * dyi  * grady( (b-bmean) )*dyi \
+                     + gradz( interpz(v-vmean) ) * dzi4 * gradz( interpyz(b-bmean) )*dzi4)
 
 printStencil(bv_shear, rhs_shear, "-=", "int", "[k]")
 
@@ -87,7 +59,7 @@ printStencil(bv_turb, rhs_turb, "-=", "top", "[k]")
 
 printEmptyLine(6)
 
-printStencil(bv_buoy, rhs_buoy, "s=", "int", "[k]")
+printStencil(bv_strat, rhs_strat, "-=", "int", "[k]")
 
 printEmptyLine(6)
 
@@ -107,4 +79,12 @@ printStencil(bv_visc, rhs_visc, "+=", "top", "[k]")
 
 printEmptyLine(6)
 
-printStencil(bv_pres, rhs_pres, "-=", "int", "[k]")
+printStencil(bv_diss, rhs_diss, "-=", "bot", "[k]")
+printEmptyLine(3)
+printStencil(bv_diss, rhs_diss, "-=", "bot+1", "[k]")
+printEmptyLine(3)
+printStencil(bv_diss, rhs_diss, "-=", "int", "[k]")
+printEmptyLine(3)
+printStencil(bv_diss, rhs_diss, "-=", "top-1", "[k]")
+printEmptyLine(3)
+printStencil(bv_diss, rhs_diss, "-=", "top", "[k]")
